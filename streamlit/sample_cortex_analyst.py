c25fac13c1b9dde8f23abe112def2015a14dea69
euphyfzhang <euphyfzhang@gmail.com>
2025-04-02 14:08:35 -0400

    debug

--- streamlit/sample_cortex_analyst.py
+++ streamlit/sample_cortex_analyst.py
@@ -230,6 +230,7 @@ def get_analyst_response(messages):
     # Check if the response is successful
     if response.status_code < 400:
         # Return the content of the response as a JSON object
+        st.write(response.content)
         content = parsed_response_message(response.content)
         return content, None
     else:

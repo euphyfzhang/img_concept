c2884f629b3043c284010e72b24c79bd45997e14
euphyfzhang <euphyfzhang@gmail.com>
2025-04-03 09:58:08 -0400

    added image

--- streamlit/sample_cortex_analyst.py
+++ streamlit/sample_cortex_analyst.py
@@ -439,7 +439,7 @@ if __name__ == "__main__":
         reset_session_state()
     
     ### HEADER AREA
-    _, banner_container, _ = st.columns([1, 9, 1], vertical_alignment="center")
+    _, banner_container, _ = st.columns([1, 9, 1], vertical_alignment="center", border = True)
     # Set the title and introductory text of the app
     with banner_container:
         st.image(banner_image, width = 600, caption = "by Euphemia")

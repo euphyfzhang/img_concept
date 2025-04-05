2b9dcf6dc0957c8dc0cc0b977a30a3335a7a8f01
euphyfzhang <euphyfzhang@gmail.com>
2025-04-04 20:21:07 -0400

    debug1

--- streamlit/snapledger.py
+++ streamlit/snapledger.py
@@ -9,7 +9,7 @@ from snowflake.snowpark import Session
 from landingai.predict import Predictor
 
 ### Release info
-release_version = "Release-1.0.10 [2025-04-04]"
+release_version = "Release-1.0.11 [2025-04-04]"
 
 ### Open config.yaml file.
 with open("streamlit/config.yaml", "r") as file:

5cda1d2f9c35618134f7b63abd49566273936218
euphyfzhang <euphyfzhang@gmail.com>
2025-04-04 20:06:57 -0400

    debug

--- streamlit/snapledger.py
+++ streamlit/snapledger.py
@@ -223,10 +223,8 @@ def parsed_response_message(content, cortex_type):
     #if cortex_type == "agent":
         #cleaned_response = re.sub(r"[']+", cleaned_response)
 
-    for each in cleansed_message:
+    for each in cleaned_response:
         st.subheader("here: ", each)
-
-    session.sql(f"INSERT INTO RESUME_AI_DB.IMG_RECG.LOG(MESSAGE) VALUES ('{cleaned_response}');").collect()
 
     #debug purpose
     parsed_list = []
